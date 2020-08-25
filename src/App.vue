dd081b911267d9975c1039e2b541b47643800f80
peipi <2390574106@qq.com>
2020-08-25 11:48:37 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,56 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <el-container>
       <el-header>
         <p>LOGO</p>
       </el-header>
-      <el-container class="ctr">
-        <el-aside class="aside-left" width="100px">
-          <el-col class="img">
-            <el-avatar shape="square" :size="40"></el-avatar>
-            <el-avatar shape="square" :size="40"></el-avatar>
-            <el-avatar shape="square" :size="40"></el-avatar>
-            <el-avatar shape="square" :size="40"></el-avatar>
-          </el-col>
-        </el-aside>
-        <el-aside class="aside-right" width="200px">
-          <el-col>
-            <el-menu default-active="2" class="el-menu-vertical-demo">
-              <el-menu-item index="1">
-                <span slot="title">二级导航</span>
-              </el-menu-item>
+      <el-container>
+        <el-aside>
+            <el-menu class="el-menu-vertical-demo">
+              <el-submenu index="1" min-width="150px">
+                <template slot="title">
+                  <span>
+                    <el-avatar shape="square" :size="40"></el-avatar>
+                  </span>
+                </template>
+                <el-menu-item-group>
+                  <router-link to="one"><el-menu-item index="1-1">二级菜单1</el-menu-item></router-link>
+                  <router-link to="two"><el-menu-item index="1-2">二级菜单2</el-menu-item></router-link>
+                  <router-link to="three"><el-menu-item index="1-3">二级菜单3</el-menu-item></router-link>
+                  <router-link to="four"><el-menu-item index="1-4">二级菜单4</el-menu-item></router-link>
+                </el-menu-item-group>
+              </el-submenu>
               <el-menu-item index="2">
-                <span slot="title">二级导航</span>
+                <span>
+                  <el-avatar shape="square" :size="40"></el-avatar>
+                </span>
               </el-menu-item>
               <el-menu-item index="3">
-                <span slot="title">二级导航</span>
+                <span>
+                  <el-avatar shape="square" :size="40"></el-avatar>
+                </span>
               </el-menu-item>
               <el-menu-item index="4">
-                <span slot="title">二级导航</span>
+                <span>
+                  <el-avatar shape="square" :size="40"></el-avatar>
+                </span>
               </el-menu-item>
             </el-menu>
-          </el-col>
         </el-aside>
-        <el-main></el-main>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
-    <div class="clear"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
-
+ 
 <style>
-.clear {
-  clear: both;
-}
+/* body */
 body {
   margin: 10px;
   background-color: #f9fafc;
@@ -62,8 +59,10 @@ body {
   background-color: #e5e9f2;
   width: 100%;
   border-radius: 10px;
+  height: 500px;
+  overflow: hidden;
 }
-/* 头部 */
+/* header*/
 .el-header {
   background-color: #324057;
   color: #f9fafc;
@@ -76,42 +75,44 @@ p {
   float: left;
   margin-left: 30px;
 }
-/* 主体部分 */
-.ctr {
-  height: 500px;
-}
+/* aside */
 .el-aside {
-  background-color: white;
+  background-color: #e5e9f2;
   color: #333;
   text-align: center;
   line-height: 200px;
+  overflow: hidden;
+  width: 280px;
 }
-.el-main {
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-body > .el-container {
-  margin-bottom: 40px;
-}
-/* 左 */
-.aside-left {
-  margin-right: 5px;
+.el-menu{
+  width: 90px;
+  position:absolute;
+  height:440px;
   border-radius: 0 0 0 10px;
 }
-.img {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+li{
+  height: 56px;
 }
-.el-avatar {
-  margin-top: 20px;
-}
-/* 右 */
-.el-main {
+.el-menu-item-group>ul{
+  position: relative;
+  left: 100px;
+  top: -70px;
   background-color: white;
-  margin: 20px;
+  height: 440px;
+  width: 200px;
+  min-width: 0;
+  overflow: hidden;
+}
+a{
+  text-decoration: none;
+  color:black;
+}
+/* 小箭头隐藏 */
+.el-submenu__icon-arrow::before{
+  display: none;
+}
+/* main */
+.el-main{
+  padding: 0px;
 }
 </style>
