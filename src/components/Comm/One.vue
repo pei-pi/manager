cf43278a2bd1123d6931b9fbf07161d3ce3d7934
peipi <2390574106@qq.com>
2020-08-26 12:16:14 +0800

    添加文件

---
+++ src/components/Comm/One.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main">我是二级菜单1</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.main {
+  background-color: white;
+margin: 10px;
+height: 420px;
+  color: #333;
+  text-align: center;
+} 
+</style>
